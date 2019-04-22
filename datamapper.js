b049d057a6c577793ccd77ae37e8921f304835f2
samhudis <samhudisnyc@gmail.com>
2019-04-22 01:09:29 -0400

    data table works

--- datamapper.js
+++ datamapper.js
@@ -1,4 +1,5 @@
 let ED = {};
+let EDLastClicked = 0;
 const ElectDistJSON = jQuery.getJSON('./gis_temp/pa_eds_lite.geo.json').then((responseJSON) => {
 const features = responseJSON.features;
 for (let i=0; i < features.length; i++) {
@@ -6,6 +7,74 @@ for (let i=0; i < features.length; i++) {
     ED[ed] = features[i];
 }
 })
+let data = {};
+let columns = [];
+let total = {};
+let totalVotes = 0;
+let candidates = [];
+$.ajax({
+    type: "GET",
+    url: "Public_advocate.csv",
+    dataType: "text",
+    success: function(response) {loadCSV(response)}
+})
+
+function htmlSanitize(str) {
+    const invalids = [".","'","'","(",")","."]
+    for (let i=0; i<invalids.length; i++) {
+        str = str.replace(invalids[i],"")
+    }
+    return str
+}
+
+function loadCSV(csv) {
+    let rows = csv.split("\n")
+    columns = rows[0].split(",");
+    for (let i=0; i<columns.length; i++) {
+        total[columns[i]] = 0;
+    }
+    for (let i=1; i<rows.length; i++) {
+        let rowArr = rows[i].split(",");
+        let row = {}
+        for (let j=0; j<columns.length;j++) {
+            row[columns[j]] = rowArr[j]
+            if (!(isNaN(Number(row[columns[j]])))) {
+                total[columns[j]] += Number(row[columns[j]])
+  
+            }
+        }
+        let ed = rowArr[0];
+        data[ed] = row;
+    }
+    // for (let i=0; i<columns.length; i++) {
+    //     totals
+    //     for (let j=0; j<data.keys.length; j++) {
+
+    //     }
+    // }
+    let table = d3.select("tbody")
+    for (let i=0; i<columns.length; i++) {
+        let column = columns[i]
+        if (!(column.indexOf("(") === -1)
+        && !(column.indexOf(")") === -1)
+        && (column.indexOf("__") === -1)
+        && !(column.indexOf("p_") === 0)
+        ) {
+            candidates.push(column)
+            let row = table.append("tr").attr("id", htmlSanitize(column)).attr("class", "candidate-row");
+            row.append("td").attr("id","key").text(column.split("(")[0]);
+            row.append("td").attr("id","value").text(total[column]);
+        }
+    }
+    for (let i=0;i<candidates.length;i++) {
+        totalVotes += total[candidates[i]]
+    }
+    let row = table.append("tr").attr("id", "total-row");
+    row.append("td").attr("id","key").text("Total");
+    row.append("td").attr("id","value").text(totalVotes)
+}
+
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtaHVkaXMiLCJhIjoiY2pudzdwZTIyMDA2dTN2bWVtY3Q1Znc5NSJ9.KKMNkGea2HILNgmDRDDj9Q';
 var map = new mapboxgl.Map({
 container: 'map',
@@ -105,6 +174,14 @@ map.on('load', function() {
             hoveredId = e.features[0].id;
             currentED = e.features[0].properties.ed;
             d3.select("#table-title").text("ED "+currentED)
+            fields = d3.selectAll(".candidate-row").attr("class","candidate-row-to-update")
+            let EDTotal = 0
+            for (let i=0; i<candidates.length; i++) {
+                let EDCandidateCount = data[currentED][candidates[i]]
+                EDTotal += Number(EDCandidateCount)
+                d3.select(".candidate-row-to-update").attr("class","candidate-row").select("#value").text(EDCandidateCount)
+            }
+            d3.select("#total-row").select("#value").text(EDTotal)
             map.setFeatureState({sourceLayer: inSourceLayer, source: 'Election District Border', id: hoveredId},{ hover: true});
         }
     })
@@ -116,11 +193,23 @@ map.on('load', function() {
         hoveredId = null;
         currentED = null;
         d3.select("#table-title").text("Total")
+        d3.selectAll(".candidate-row").attr("class", "candidate-row-to-update")
+        for (let i=0; i<candidates.length;i++) {
+            d3.select(".candidate-row-to-update").attr("class","candidate-row").select("#value").text(total[candidates[i]])
+        }
+        d3.select("#total-row").select("#value").text(totalVotes)
+
     })
 
     map.on('click', 'Election District', function (e) {
-        // map.jumpTo({zoom: 12});
-        zoomToED(map, e.features[0].properties.ed);
-
+        let clickedED = e.features[0].properties.ed;
+        if (clickedED === EDLastClicked) {
+            EDLastClicked = null;
+            zoomToCity(map);
+        }
+        else {
+        EDLastClicked = clickedED;
+        zoomToED(map, clickedED);
+        }
         });
 })
